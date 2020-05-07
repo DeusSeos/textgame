6d22e9c49add5cb5b570f87bf7cf183b666dca96
Seos <dchua200@gmail.com>
2020-05-06 17:45:43 -0700

    Finished location array creation

--- LocationExtractor.py
+++ LocationExtractor.py
@@ -1,11 +1,12 @@
 
 
 class Location():
-    def __init__(self, locationName, welcomeMessage, objects, connectingLocations):
+    def __init__(self, locationName, welcomeMessage, objects, connectingLocations, mobs):
         self.location = locationName
         self.welcomeMessage = welcomeMessage
         self.objects = objects
         self.connectingLocations = connectingLocations
+        self.mobs = mobs
 
     def getLocation(self):
         return self.location
@@ -19,8 +20,11 @@ class Location():
     def getConnectingLocations(self):
         return self.connectingLocations
 
+    def getMobs(self):
+        return self.mobs
+
     def __str__(self):
-        return "{}||{}||{}||{}".format(self.location, self.welcomeMessage, self.objects, self.connectingLocations)
+        return "{}||{}||{}||{}||{}".format(self.location, self.welcomeMessage, self.objects, self.connectingLocations,self.mobs)
 
 
 
--- Main.py
+++ Main.py
@@ -1,23 +1,62 @@
 # This is the driving code for creating the different objects
 # Special characters to indicate end of a section and end of a location
-# End of section: ||
 # End of location: ***
 
-readDataList = []
 
-try: #checks to see if file exists or not
-    with open('Location.txt') as fileInit:
-        if (fileInit.read() == ''):
-            print('File exists but is empty. Will append structure to file')
-        else:
-            print("File exists. Structure hasn't been checked.")
-except:
-    print("File doesn't exist ")
-    fileInit = open('Location.txt', 'w+')
-    fileInit.write("Location:\nWelcome Message:\nObjects:\nConnecting Locations:\nMobs:\n***")
+import LocationExtractor
+import Mob
 
-with open('Location.txt') as file:
-    for line in file:
-        readDataList.append(line)
+
+def initalize(fileName = 'Location.txt'):
+    wordsRemove = ['Location:', 'Welcome Message:', 'Objects:', 'Connecting Locations:', 'Mobs:']
+    readDataList = []
+    locations = []
+    locationTotal = 0
+
+    try: #checks to see if file exists or not
+        with open('Location.txt') as fileInit:
+            if (fileInit.read() == ''):
+                print('File exists but is empty. Will append structure to file')
+            else:
+                print("File exists. Structure hasn't been checked.")
+    except:
+        print("File doesn't exist ")
+        fileInit = open('Location.txt', 'w+')
+        fileInit.write("Location:\nWelcome Message:\nObjects:\nConnecting Locations:\nMobs:\n***\n")
+        fileInit.close()
+    with open(fileName) as file:
+        for line in file:
+            readDataList.append(line.replace('\n', '').strip())
+    for index, element in enumerate(readDataList): #check total location and cleaning up the location syntax before stuffing into array
+        if element == '***':
+            locationTotal += 1
+        for word in wordsRemove:
+            if word in element:
+                readDataList[index] = element.replace(word, '')
+                break
+
+    index = 0
+    i=0
+
+    while i < locationTotal:
+        location = readDataList[index]
+        welcomeMessage = readDataList[index+1]
+        objects = readDataList[index+2].split(',')
+        connectingLocations = readDataList[index+3].split(',')
+        mobs = readDataList[index+4].split(',')
+        a = LocationExtractor.Location(location, welcomeMessage, objects, connectingLocations, mobs)
+        locations.append(a)
+        index += 6
+        i += 1
+
+    return locations
+
+locationArray = initalize()
+
+for element in locationArray:
+    print(str(element))
+
+
+
 
 
